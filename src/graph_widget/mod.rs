32c0982440ac5385b9f311f6053d1e19c12ee14f
Romeo Calota <mail@romeocalota.me>
2023-04-30 10:58:46 +0300

    I can't remember what I did here

--- src/graph_widget/mod.rs
+++ src/graph_widget/mod.rs
@@ -84,54 +84,31 @@ mod imp {
             width: i32,
             height: i32,
         ) -> Result<(), Box<dyn std::error::Error>> {
+            use plotters::prelude::*;
+
             let backend =
                 skia_plotter_backend::SkiaPlotterBackend::new(canvas, width as _, height as _);
             let root = plotters::drawing::IntoDrawingArea::into_drawing_area(backend);
 
-            let draw_func = move || -> Result<(), Box<dyn std::error::Error>> {
-                use plotters::prelude::*;
+            // After this point, we should be able to construct a chart context
+            let mut chart = ChartBuilder::on(&root)
+                // Set the caption of the chart
+                // Set the size of the label region
+                .x_label_area_size(0)
+                .y_label_area_size(0)
+                // Finally attach a coordinate on the drawing area and make a chart context
+                .build_cartesian_2d(0_f32..60., 0_f32..100.)?;
 
-                // After this point, we should be able to construct a chart context
-                let mut chart = ChartBuilder::on(&root)
-                    // Set the caption of the chart
-                    .caption("This is our first plot", ("sans-serif", 40).into_font())
-                    // Set the size of the label region
-                    .x_label_area_size(20)
-                    .y_label_area_size(40)
-                    // Finally attach a coordinate on the drawing area and make a chart context
-                    .build_cartesian_2d(0f32..10f32, 0f32..10f32)?;
+            // Then we can draw a mesh
+            chart
+                .configure_mesh()
+                // We can customize the maximum number of labels allowed for each axis
+                .x_labels(60)
+                .y_labels(100)
+                // We can also change the format of the label text
+                .draw()?;
 
-                // Then we can draw a mesh
-                chart
-                    .configure_mesh()
-                    // We can customize the maximum number of labels allowed for each axis
-                    .x_labels(5)
-                    .y_labels(5)
-                    // We can also change the format of the label text
-                    .y_label_formatter(&|x| format!("{:.3}", x))
-                    .draw()?;
-
-                // And we can draw something in the drawing area
-                chart.draw_series(LineSeries::new(
-                    vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
-                    &RED,
-                ))?;
-                // Similarly, we can draw point series
-                chart.draw_series(PointSeries::of_element(
-                    vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
-                    5,
-                    &RED,
-                    &|c, s, st| {
-                        return EmptyElement::at(c)    // We want to construct a composed element on-the-fly
-                            + Circle::new((0, 0), s, st.filled()) // At this point, the new pixel coordinate is established
-                            + Text::new(format!("{:?}", c), (10, 0), ("sans-serif", 10).into_font());
-                    },
-                ))?;
-                root.present()?;
-
-                Ok(())
-            };
-            draw_func().unwrap();
+            root.present()?;
 
             Ok(())
         }
