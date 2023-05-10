a03bd8afa6446667f6b5dd9070e96efd7fe00bee
Romeo Calota <mail@romeocalota.me>
2023-05-10 08:38:37 +0300

    Implement missing properties in NetInfo

--- src/sys_info/network.rs
+++ src/sys_info/network.rs
@@ -18,14 +18,15 @@ pub struct NetworkDeviceDescriptor {
 pub struct Address {
     pub hw_address: Option<[u8; 6]>,
     pub ip4_address: Option<u32>,
-    pub ip6_address: Option<u32>,
+    pub ip6_address: Option<u128>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct WirelessInfo {
-    pub ssid: String,
-    pub connection_type: String,
-    pub signal_strength: u8,
+    pub ssid: Option<String>,
+    pub frequency: Option<u32>,
+    pub bitrate: Option<u32>,
+    pub signal_strength: Option<u8>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -121,9 +122,9 @@ impl NetInfo {
         for device_if in devices {
             let if_name = device_if.into();
 
-            if let Some(device_path) = unsafe { self.nm_device_obj_path(if_name) } {
+            if let Some(device_path) = unsafe { self.nm_obj_path_new(if_name) } {
                 let device_proxy = unsafe {
-                    self.create_nm_dbus_proxy(
+                    Self::create_nm_dbus_proxy(
                         device_path.as_bytes_with_nul(),
                         b"org.freedesktop.NetworkManager.Device\0",
                     )
@@ -136,6 +137,8 @@ impl NetInfo {
                 let r#type = Self::device_type(if_name);
                 let adapter_name = unsafe { self.adapter_name(device_proxy) };
                 let hw_address = Self::hw_address(device_proxy);
+                let ip4_address = unsafe { Self::ip4_address(device_proxy) };
+                let ip6_address = unsafe { Self::ip6_address(device_proxy) };
 
                 unsafe { g_object_unref(device_proxy as _) };
 
@@ -148,10 +151,17 @@ impl NetInfo {
 
                 let address = Address {
                     hw_address,
-                    ip4_address: None,
-                    ip6_address: None,
+                    ip4_address,
+                    ip6_address,
                 };
                 dbg!(address);
+
+                let wireless_info = if r#type == NetDeviceType::Wireless {
+                    unsafe { Self::wireless_info(device_proxy) }
+                } else {
+                    None
+                };
+                dbg!(wireless_info);
             }
         }
 
@@ -170,7 +180,7 @@ impl NetInfo {
 
     fn hw_address(dbus_proxy: *mut gtk::gio::ffi::GDBusProxy) -> Option<[u8; 6]> {
         if let Some(hw_address_variant) =
-            unsafe { Self::nm_device_get_property(dbus_proxy, b"HwAddress\0") }
+            unsafe { Self::nm_device_property(dbus_proxy, b"HwAddress\0") }
         {
             if let Some(hw_address_str) = hw_address_variant.str() {
                 let mut hw_address = [0; 6];
@@ -191,6 +201,174 @@ impl NetInfo {
         }
     }
 
+    unsafe fn ip4_address(dbus_proxy: *mut gtk::gio::ffi::GDBusProxy) -> Option<u32> {
+        use gtk::glib::gobject_ffi::*;
+
+        if let Some(ip4_address_obj_path) = Self::nm_device_property(dbus_proxy, b"Ip4Config\0") {
+            if let Some(ip4_address_obj_path_str) = ip4_address_obj_path.str() {
+                let ip4_config_proxy = Self::create_nm_dbus_proxy(
+                    ip4_address_obj_path_str.as_bytes(),
+                    b"org.freedesktop.NetworkManager.IP4Config\0",
+                );
+                if ip4_config_proxy.is_null() {
+                    return None;
+                }
+
+                let result = if let Some(ip4_address_variant) =
+                    Self::nm_device_property(ip4_config_proxy, b"Addresses\0")
+                {
+                    // Just take the first entry in the list of lists
+                    if let Some(ip4_address_info) = ip4_address_variant.iter().next() {
+                        // The first entry in the inner list is the IP address
+                        ip4_address_info
+                            .iter()
+                            .next()
+                            .map_or(None, |v| v.get::<u32>())
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                };
+
+                g_object_unref(ip4_config_proxy as _);
+
+                result
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    unsafe fn ip6_address(dbus_proxy: *mut gtk::gio::ffi::GDBusProxy) -> Option<u128> {
+        use gtk::glib::gobject_ffi::*;
+
+        if let Some(ip6_address_obj_path) = Self::nm_device_property(dbus_proxy, b"Ip6Config\0") {
+            if let Some(ip6_address_obj_path_str) = ip6_address_obj_path.str() {
+                let ip6_config_proxy = Self::create_nm_dbus_proxy(
+                    ip6_address_obj_path_str.as_bytes(),
+                    b"org.freedesktop.NetworkManager.IP6Config\0",
+                );
+                if ip6_config_proxy.is_null() {
+                    return None;
+                }
+
+                let result = if let Some(ip6_address_variant) =
+                    Self::nm_device_property(ip6_config_proxy, b"Addresses\0")
+                {
+                    // Just take the first entry in the list of lists
+                    if let Some(ip6_address_info) = ip6_address_variant.iter().next() {
+                        ip6_address_info.iter().next().map_or(None, |v| {
+                            let mut ip6_address = [0; 16];
+                            v.iter().enumerate().for_each(|(i, v)| {
+                                ip6_address[i] = v.get::<u8>().unwrap_or(0);
+                            });
+
+                            Some(u128::from_be_bytes(ip6_address))
+                        })
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                };
+
+                g_object_unref(ip6_config_proxy as _);
+
+                result
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    unsafe fn wireless_info(dbus_proxy: *mut gtk::gio::ffi::GDBusProxy) -> Option<WirelessInfo> {
+        use gtk::{gio::ffi::*, glib::gobject_ffi::*};
+
+        use std::ffi::CStr;
+
+        let wireless_obj_path = CStr::from_ptr(g_dbus_proxy_get_object_path(dbus_proxy));
+
+        let wireless_info_proxy = Self::create_nm_dbus_proxy(
+            wireless_obj_path.to_bytes_with_nul(),
+            b"org.freedesktop.NetworkManager.Device.Wireless\0",
+        );
+        if wireless_info_proxy.is_null() {
+            return None;
+        }
+
+        let result = if let Some(wireless_info_variant) =
+            Self::nm_device_property(wireless_info_proxy, b"ActiveAccessPoint\0")
+        {
+            if let Some(wireless_info_obj_path) = wireless_info_variant.str() {
+                let wireless_info_proxy = Self::create_nm_dbus_proxy(
+                    wireless_info_obj_path.as_bytes(),
+                    b"org.freedesktop.NetworkManager.AccessPoint\0",
+                );
+                if wireless_info_proxy.is_null() {
+                    return None;
+                }
+
+                let ssid = if let Some(ssid_variant) =
+                    Self::nm_device_property(wireless_info_proxy, b"Ssid\0")
+                {
+                    let ssid = ssid_variant
+                        .iter()
+                        .filter_map(|v| v.get::<u8>())
+                        .collect::<Vec<_>>();
+
+                    String::from_utf8(ssid).ok()
+                } else {
+                    None
+                };
+
+                let frequency = if let Some(frequency) =
+                    Self::nm_device_property(wireless_info_proxy, b"Frequency\0")
+                {
+                    frequency.get::<u32>()
+                } else {
+                    None
+                };
+
+                let bitrate = if let Some(bitrate) =
+                    Self::nm_device_property(wireless_info_proxy, b"MaxBitrate\0")
+                {
+                    bitrate.get::<u32>()
+                } else {
+                    None
+                };
+
+                let signal_strength = if let Some(signal_strength) =
+                    Self::nm_device_property(wireless_info_proxy, b"Strength\0")
+                {
+                    signal_strength.get::<u8>()
+                } else {
+                    None
+                };
+
+                g_object_unref(wireless_info_proxy as _);
+                Some(WirelessInfo {
+                    ssid,
+                    frequency,
+                    bitrate,
+                    signal_strength,
+                })
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        g_object_unref(wireless_info_proxy as _);
+
+        result
+    }
+
     unsafe fn adapter_name(&self, dbus_proxy: *mut gtk::gio::ffi::GDBusProxy) -> Option<String> {
         extern "C" {
             fn strerror(error: i32) -> *const i8;
@@ -202,7 +380,7 @@ impl NetInfo {
 
         use std::ffi::CStr;
 
-        if let Some(udi_variant) = Self::nm_device_get_property(dbus_proxy, b"Udi\0") {
+        if let Some(udi_variant) = Self::nm_device_property(dbus_proxy, b"Udi\0") {
             if let Some(udi) = udi_variant.str() {
                 let udev_device = udev_device_new_from_syspath(self.udev, udi.as_ptr() as _);
                 if udev_device.is_null() {
@@ -250,7 +428,6 @@ impl NetInfo {
     }
 
     unsafe fn create_nm_dbus_proxy(
-        &self,
         path: &[u8],
         interface: &[u8],
     ) -> *mut gtk::gio::ffi::GDBusProxy {
@@ -291,7 +468,7 @@ impl NetInfo {
         proxy
     }
 
-    unsafe fn nm_device_obj_path(&self, device_if: &str) -> Option<std::ffi::CString> {
+    unsafe fn nm_obj_path_new(&self, device_if: &str) -> Option<std::ffi::CString> {
         use gtk::gio::ffi::*;
         use gtk::glib::{ffi::*, translate::from_glib_full, Error, *};
         use std::ffi::{CStr, CString};
@@ -342,16 +519,17 @@ impl NetInfo {
         }
     }
 
-    unsafe fn nm_device_get_property(
+    unsafe fn nm_device_property(
         dbus_proxy: *mut gtk::gio::ffi::GDBusProxy,
         property: &[u8],
     ) -> Option<gtk::glib::Variant> {
         use gtk::gio::ffi::*;
         use gtk::glib::{ffi::*, translate::from_glib_full, Error, *};
+        use std::ffi::CStr;
 
         let mut error: *mut GError = std::ptr::null_mut();
 
-        let udi_variant = g_dbus_proxy_call_sync(
+        let variant = g_dbus_proxy_call_sync(
             dbus_proxy,
             b"org.freedesktop.DBus.Properties.Get\0".as_ptr() as _,
             g_variant_new(
@@ -364,41 +542,44 @@ impl NetInfo {
             std::ptr::null_mut(),
             &mut error,
         );
-        if udi_variant.is_null() {
+        if variant.is_null() {
             if !error.is_null() {
                 let error: Error = from_glib_full(error);
                 g_error!(
                     "MissionCenter::NetInfo",
-                    "Failed to get udev device path: {}",
+                    "Failed to get property {:?}: {}",
+                    CStr::from_ptr(property.as_ptr() as _),
                     error.message()
                 );
             } else {
                 g_error!(
                     "MissionCenter::NetInfo",
-                    "Failed to get udev device path: Unknown error"
+                    "Failed to get property {:?}: Unknown error",
+                    CStr::from_ptr(property.as_ptr() as _),
                 );
             }
 
             return None;
         }
 
-        let mut udi_inner: *mut GVariant = std::ptr::null_mut();
-        g_variant_get(udi_variant, b"(v)\0".as_ptr() as _, &mut udi_inner);
-        if udi_inner.is_null() {
-            g_variant_unref(udi_variant);
+        let mut inner: *mut GVariant = std::ptr::null_mut();
+        g_variant_get(variant, b"(v)\0".as_ptr() as _, &mut inner);
+        if inner.is_null() {
+            g_variant_unref(variant);
 
             g_error!(
                 "MissionCenter::NetInfo",
-                "Failed to get udev device path, cannot extract inner variant: Unknown error"
+                "Failed to get property {:?}, cannot extract inner variant: Unknown error",
+                CStr::from_ptr(property.as_ptr() as _),
             );
 
             return None;
         }
 
-        g_variant_ref_sink(udi_inner);
-        g_variant_unref(udi_variant);
+        g_variant_ref_sink(inner);
+        g_variant_unref(variant);
 
-        from_glib_full(udi_inner)
+        from_glib_full(inner)
     }
 
     // Yanked from NetworkManager: src/libnm-client-impl/nm-device.c: _get_udev_property()
