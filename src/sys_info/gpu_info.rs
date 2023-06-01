055bd4a4ecb6fe11667d4a0c08cb3ce44d94df84
Romeo Calota <mail@romeocalota.me>
2023-06-01 09:37:25 +0300

    Remove GPUd, at least for the time being, also skip Intel integrated graphics, they are useless in their curent form

--- src/sys_info/gpu_info.rs
+++ src/sys_info/gpu_info.rs
@@ -19,8 +19,199 @@
  */
 
 #[allow(unused)]
-mod shm {
-    include!("../../gpud/src/shm.rs");
+mod nvtop {
+    const MAX_DEVICE_NAME: usize = 128;
+    const PDEV_LEN: usize = 16;
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub struct ListHead {
+        pub next: *mut ListHead,
+        pub prev: *mut ListHead,
+    }
+
+    unsafe impl Send for ListHead {}
+
+    unsafe impl Sync for ListHead {}
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub struct GPUVendor {
+        pub list: ListHead,
+
+        pub init: Option<fn() -> u8>,
+        pub shutdown: Option<fn()>,
+
+        pub last_error_string: Option<extern "C" fn() -> *const i8>,
+
+        pub get_device_handles:
+            Option<extern "C" fn(devices: *mut ListHead, count: *mut u32) -> u8>,
+
+        pub populate_static_info: Option<extern "C" fn(gpu_info: *mut GPUInfo) -> u8>,
+        pub refresh_dynamic_info: Option<extern "C" fn(gpu_info: *mut GPUInfo) -> u8>,
+
+        pub refresh_running_processes: Option<extern "C" fn(gpu_info: *mut GPUInfo) -> u8>,
+
+        pub name: *mut i8,
+    }
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub enum GPUInfoStaticInfoValid {
+        DeviceNameValid = 0,
+        MaxPcieGenValid,
+        MaxPcieLinkWidthValid,
+        TemperatureShutdownThresholdValid,
+        TemperatureSlowdownThresholdValid,
+        StaticInfoCount,
+    }
+
+    const GPU_INFO_STATIC_INFO_COUNT: usize = GPUInfoStaticInfoValid::StaticInfoCount as usize;
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub struct GPUInfoStaticInfo {
+        pub device_name: [i8; MAX_DEVICE_NAME],
+        pub max_pcie_gen: u32,
+        pub max_pcie_link_width: u32,
+        pub temperature_shutdown_threshold: u32,
+        pub temperature_slowdown_threshold: u32,
+        pub integrated_graphics: u8,
+        pub valid: [u8; (GPU_INFO_STATIC_INFO_COUNT + 7) / 8],
+    }
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub enum GPUInfoDynamicInfoValid {
+        GpuClockSpeedValid = 0,
+        GpuClockSpeedMaxValid,
+        MemClockSpeedValid,
+        MemClockSpeedMaxValid,
+        GpuUtilRateValid,
+        MemUtilRateValid,
+        EncoderRateValid,
+        DecoderRateValid,
+        TotalMemoryValid,
+        FreeMemoryValid,
+        UsedMemoryValid,
+        PcieLinkGenValid,
+        PcieLinkWidthValid,
+        PcieRxValid,
+        PcieTxValid,
+        FanSpeedValid,
+        GpuTempValid,
+        PowerDrawValid,
+        PowerDrawMaxValid,
+        DynamicInfoCount,
+    }
+
+    const GPU_INFO_DYNAMIC_INFO_COUNT: usize = GPUInfoDynamicInfoValid::DynamicInfoCount as usize;
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub struct GPUInfoDynamicInfo {
+        pub gpu_clock_speed: u32,
+        pub gpu_clock_speed_max: u32,
+        pub mem_clock_speed: u32,
+        pub mem_clock_speed_max: u32,
+        pub gpu_util_rate: u32,
+        pub mem_util_rate: u32,
+        pub encoder_rate: u32,
+        pub decoder_rate: u32,
+        pub total_memory: u64,
+        pub free_memory: u64,
+        pub used_memory: u64,
+        pub pcie_link_gen: u32,
+        pub pcie_link_width: u32,
+        pub pcie_rx: u32,
+        pub pcie_tx: u32,
+        pub fan_speed: u32,
+        pub gpu_temp: u32,
+        pub power_draw: u32,
+        pub power_draw_max: u32,
+        pub encode_decode_shared: u8,
+        pub valid: [u8; (GPU_INFO_DYNAMIC_INFO_COUNT + 7) / 8],
+    }
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub enum GPUProcessType {
+        Unknown = 0,
+        Graphical = 1,
+        Compute = 2,
+        GraphicalCompute = 3,
+        Count,
+    }
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub enum GPUInfoProcessInfoValid {
+        CmdlineValid,
+        UserNameValid,
+        GfxEngineUsedValid,
+        ComputeEngineUsedValid,
+        EncEngineUsedValid,
+        DecEngineUsedValid,
+        GpuUsageValid,
+        EncodeUsageValid,
+        DecodeUsageValid,
+        GpuMemoryUsageValid,
+        GpuMemoryPercentageValid,
+        CpuUsageValid,
+        CpuMemoryVirtValid,
+        CpuMemoryResValid,
+        ProcessValidInfoCount,
+    }
+
+    const GPU_PROCESS_INFO_VALID_INFO_COUNT: usize =
+        GPUInfoProcessInfoValid::ProcessValidInfoCount as usize;
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub struct GPUProcess {
+        pub r#type: GPUProcessType,
+        pub pid: i32,
+        pub cmdline: *mut i8,
+        pub user_name: *mut i8,
+        pub gfx_engine_used: u64,
+        pub compute_engine_used: u64,
+        pub enc_engine_used: u64,
+        pub dec_engine_used: u64,
+        pub gpu_usage: u32,
+        pub encode_usage: u32,
+        pub decode_usage: u32,
+        pub gpu_memory_usage: libc::c_ulonglong,
+        pub gpu_memory_percentage: u32,
+        pub cpu_usage: u32,
+        pub cpu_memory_virt: libc::c_ulong,
+        pub cpu_memory_res: libc::c_ulong,
+        pub valid: [u8; (GPU_PROCESS_INFO_VALID_INFO_COUNT + 7) / 8],
+    }
+
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub struct GPUInfo {
+        pub list: ListHead,
+        pub vendor: *mut GPUVendor,
+        pub static_info: GPUInfoStaticInfo,
+        pub dynamic_info: GPUInfoDynamicInfo,
+        pub processes_count: u32,
+        pub processes: *mut GPUProcess,
+        pub processes_array_size: u32,
+        pub pdev: [i8; PDEV_LEN],
+    }
+
+    extern "C" {
+        pub fn gpuinfo_init_info_extraction(
+            monitored_dev_count: *mut u32,
+            devices: *mut ListHead,
+        ) -> u8;
+
+        pub fn gpuinfo_populate_static_infos(devices: *mut ListHead) -> u8;
+        pub fn gpuinfo_refresh_dynamic_info(devices: *mut ListHead) -> u8;
+        pub fn gpuinfo_refresh_processes(devices: *mut ListHead) -> u8;
+        pub fn gpuinfo_fix_dynamic_info_from_process_info(devices: *mut ListHead) -> u8;
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -41,10 +232,7 @@ pub struct GPU {
 }
 
 pub struct GPUInfo {
-    shm: shm::SharedMemory,
-    shm_file: String,
-    gpud_process: subprocess::Popen,
-
+    gpu_list: Box<nvtop::ListHead>,
     gpus: Vec<GPU>,
 }
 
@@ -66,139 +254,106 @@ impl GPUInfo {
         std::fs::create_dir_all(&cache_dir).expect("Unable to create cache directory");
         cache_dir.push_str("/gpud_shm");
 
-        let shm = shm::SharedMemory::new(&cache_dir, true);
-        if shm.is_none() {
-            g_critical!("MissionCenter::SysInfo", "Unable to open shared memory");
-            return None;
+        let mut gpu_list = Box::new(nvtop::ListHead {
+            next: std::ptr::null_mut(),
+            prev: std::ptr::null_mut(),
+        });
+        gpu_list.next = gpu_list.as_mut();
+        gpu_list.prev = gpu_list.as_mut();
+
+        let mut gpu_count: u32 = 0;
+        unsafe {
+            let result = nvtop::gpuinfo_init_info_extraction(&mut gpu_count, gpu_list.as_mut());
+            if result == 0 {
+                g_critical!(
+                    "MissionCenter::GPUInfo",
+                    "Unable to initialize GPU info extraction"
+                );
+                return None;
+            }
+
+            let result = nvtop::gpuinfo_populate_static_infos(gpu_list.as_mut());
+            if result == 0 {
+                g_critical!(
+                    "MissionCenter::GPUInfo",
+                    "Unable to populate static GPU info"
+                );
+                return None;
+            }
         }
-        let shm = shm.unwrap();
 
-        let child = match Self::start_gpud(&cache_dir) {
-            Some(child) => child,
-            None => return None,
-        };
-
-        let mut this = Self {
-            shm,
-            shm_file: cache_dir,
-            gpud_process: child,
+        Some(Self {
+            gpu_list,
             gpus: vec![],
-        };
-
-        // Wait a bit in case the process fails to start
-        std::thread::sleep(std::time::Duration::from_millis(100));
-        match this.has_gpud_exited() {
-            None | Some(true) => return None,
-            _ => {}
-        }
-
-        Some(this)
-    }
-
-    fn start_gpud(shm_file_link: &str) -> Option<subprocess::Popen> {
-        use gtk::glib::*;
-
-        let gpud_executable = match *super::IS_FLATPAK {
-            true => "/app/bin/gpud",
-            false => "gpud",
-        };
-
-        let child = subprocess::Exec::cmd(gpud_executable)
-            .arg(&shm_file_link)
-            .detached()
-            .popen();
-        if child.is_err() {
-            g_critical!(
-                "MissionCenter::SysInfo",
-                "Unable to spawn gpud process: {}",
-                child.err().unwrap()
-            );
-            return None;
-        }
-
-        Some(child.unwrap())
+        })
     }
 
     pub fn refresh(&mut self) {
         use gtk::glib::*;
-        use raw_sync::Timeout;
-        use std::time::Duration;
 
-        match self.has_gpud_exited() {
-            None => {
-                return;
-            }
-            Some(true) => {
-                let child = Self::start_gpud(&self.shm_file);
-                if child.is_none() {
-                    return;
-                }
-                self.gpud_process = child.unwrap();
-
-                // Wait a bit in case the process fails to start
-                std::thread::sleep(std::time::Duration::from_millis(100));
-                match self.has_gpud_exited() {
-                    None | Some(true) => return,
-                    _ => {}
-                }
-            }
-            _ => {}
+        let result = unsafe { nvtop::gpuinfo_refresh_dynamic_info(self.gpu_list.as_mut()) };
+        if result == 0 {
+            g_critical!(
+                "MissionCenter::GPUInfo",
+                "Unable to refresh dynamic GPU info"
+            );
+            return;
         }
 
-        if let Some(reader) = self.shm.read(Timeout::Val(Duration::from_secs(1))) {
-            self.gpus.clear();
+        let result = unsafe { nvtop::gpuinfo_refresh_processes(self.gpu_list.as_mut()) };
+        if result == 0 {
+            g_critical!("MissionCenter::GPUInfo", "Unable to refresh GPU processes");
+            return;
+        }
 
-            for gpu in reader.gpu_info {
-                self.gpus.push(GPU {
-                    device_name: unsafe {
-                        std::ffi::CStr::from_ptr(gpu.static_info.device_name.as_ptr())
-                    }
-                    .to_string_lossy()
-                    .to_string(),
-                    dri_path: unsafe {
-                        std::ffi::CStr::from_ptr(gpu.dri_path.as_ptr() as *const i8)
-                    }
-                    .to_string_lossy()
-                    .to_string(),
-                    temp_celsius: gpu.dynamic_info.gpu_temp,
-                    fan_speed_percent: gpu.dynamic_info.fan_speed,
-                    util_percent: gpu.dynamic_info.gpu_util_rate,
-                    power_draw_watts: gpu.dynamic_info.power_draw as f32 / 1000.,
-                    clock_speed_mhz: gpu.dynamic_info.gpu_clock_speed,
-                    mem_speed_mhz: gpu.dynamic_info.mem_clock_speed,
-                    total_memory: gpu.dynamic_info.total_memory,
-                    free_memory: gpu.dynamic_info.free_memory,
-                    used_memory: gpu.dynamic_info.used_memory,
-                    encoder_percent: gpu.dynamic_info.encoder_rate,
-                    decoder_percent: gpu.dynamic_info.decoder_rate,
-                });
-            }
-        } else {
-            g_warning!(
-                "MissionCenter::SysInfo",
-                "Unable to read shared memory: Timeout while waiting for lock"
+        let result =
+            unsafe { nvtop::gpuinfo_fix_dynamic_info_from_process_info(self.gpu_list.as_mut()) };
+        if result == 0 {
+            g_critical!(
+                "MissionCenter::GPUInfo",
+                "Unable to fix dynamic GPU info from process info"
             );
+            return;
+        }
+
+        self.gpus.clear();
+
+        let mut device: *mut nvtop::ListHead = self.gpu_list.next;
+        while device != self.gpu_list.as_mut() {
+            let dev: &nvtop::GPUInfo = unsafe { core::mem::transmute(device) };
+
+            let pdev = unsafe { std::ffi::CStr::from_ptr(dev.pdev.as_ptr()) }.to_string_lossy();
+            // Skip Intel integrated graphics, stats for them are utterly broken
+            if pdev == "0000:00:02.0" {
+                device = unsafe { (*device).next };
+                continue;
+            }
+
+            self.gpus.push(GPU {
+                device_name: unsafe {
+                    std::ffi::CStr::from_ptr(dev.static_info.device_name.as_ptr())
+                }
+                .to_string_lossy()
+                .to_string(),
+                dri_path: format!("/dev/dri/by-path/pci-{}-card", pdev),
+                temp_celsius: dev.dynamic_info.gpu_temp,
+                fan_speed_percent: dev.dynamic_info.fan_speed,
+                util_percent: dev.dynamic_info.gpu_util_rate,
+                power_draw_watts: dev.dynamic_info.power_draw as f32 / 1000.,
+                clock_speed_mhz: dev.dynamic_info.gpu_clock_speed,
+                mem_speed_mhz: dev.dynamic_info.mem_clock_speed,
+                total_memory: dev.dynamic_info.total_memory,
+                free_memory: dev.dynamic_info.free_memory,
+                used_memory: dev.dynamic_info.used_memory,
+                encoder_percent: dev.dynamic_info.encoder_rate,
+                decoder_percent: dev.dynamic_info.decoder_rate,
+            });
+
+            device = unsafe { (*device).next };
         }
     }
 
     pub fn gpus(&self) -> &[GPU] {
         &self.gpus
-    }
-
-    fn has_gpud_exited(&mut self) -> Option<bool> {
-        use gtk::glib::*;
-
-        match self.gpud_process.poll() {
-            Some(status) => {
-                g_critical!(
-                    "MissionCenter::SysInfo",
-                    "gpud process exited with status: {:?}",
-                    status
-                );
-
-                Some(true)
-            }
-            None => Some(false),
-        }
     }
 }
