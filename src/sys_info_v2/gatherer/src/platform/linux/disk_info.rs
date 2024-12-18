f7c654b38c009ab37c9f0c9b8da41304b81ec4a4
jojo2357 <11406210-jojo2357@users.noreply.gitlab.com>
2024-12-18 22:55:17 +0000

    debug smore

--- src/sys_info_v2/gatherer/src/platform/linux/disk_info.rs
+++ src/sys_info_v2/gatherer/src/platform/linux/disk_info.rs
@@ -345,7 +345,7 @@ impl<'a> DisksInfoExt<'a> for LinuxDisksInfo {
             }
 
             println!("Reading thermals for {}", dir_name);
-            println!("Reading thermals {:?}", glob(format!("/sys/block/{}/device/hwmon*/temp*_input", dir_name).as_str()).unwrap().filter_map(Result::ok).map(|p| format!("{:?}", p)));
+            println!("Reading thermals {:?}", glob(format!("/sys/block/{}/device/hwmon*/temp*_input", dir_name).as_str()).unwrap().filter_map(Result::ok).map(|p| format!("{:?}", p)).collect::<Vec<_>>);
 
             let temp_inputs = glob(format!("/sys/block/{}/device/hwmon[0-9]*/temp[0-9]*_input", dir_name).as_str()).unwrap()
                 .filter_map(Result::ok)
