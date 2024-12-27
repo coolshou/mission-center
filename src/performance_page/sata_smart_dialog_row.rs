d316d908b56d9871444dd6b578fcfd119872a6e4
jojo2357 <11406210-jojo2357@users.noreply.gitlab.com>
2024-12-27 14:16:39 -0800

    cleanup

--- src/performance_page/sata_smart_dialog_row.rs
+++ src/performance_page/sata_smart_dialog_row.rs
@@ -39,7 +39,6 @@ mod imp {
     #[derive(Default, Properties)]
     #[properties(wrapper_type = super::SmartDialogRow)]
     pub struct SmartDialogRow {
-        // pub row_entry: OnceCell<gtk::ListBoxRow>,
         #[property(get, set)]
         pub smart_id: Cell<u8>,
         #[property(get, set)]
@@ -58,31 +57,11 @@ mod imp {
         pub updates: OnceCell<String>,
         #[property(get, set)]
         pub assessment: OnceCell<String>,
-        // pub value: OnceCell<gtk::Label>,
-        // pub pretty: OnceCell<gtk::Label>,
-        // pub normalized: OnceCell<gtk::Label>,
-        // pub threshold: OnceCell<gtk::Label>,
-        // pub worst: OnceCell<gtk::Label>,
     }
 
     impl SmartDialogRow {
     }
 
-/*    impl Default for SmartDialogRow {
-        fn default() -> Self {
-            Self {
-                row_entry: Default::default(),
-                id: Default::default(),
-                attribute: Default::default(),
-                value: Default::default(),
-                pretty: Default::default(),
-                normalized: Default::default(),
-                threshold: Default::default(),
-                worst: Default::default(),
-            }
-        }
-    }
-*/
     #[glib::object_subclass]
     impl ObjectSubclass for SmartDialogRow {
         const NAME: &'static str = "SmartDialogRow";
@@ -97,69 +76,6 @@ mod imp {
         }
     }
 
-/*    impl ObjectImpl for SmartDialogRow {
-        fn properties() -> &'static [ParamSpec] {
-            Self::derived_properties()
-        }
-
-        fn set_property(&self, id: usize, value: &Value, pspec: &ParamSpec) {
-            self.derived_set_property(id, value, pspec)
-        }
-
-        fn property(&self, id: usize, pspec: &ParamSpec) -> Value {
-            self.derived_property(id, pspec)
-        }
-
-        fn constructed(&self) {
-            self.parent_constructed();
-
-            let sidebar_content_builder = gtk::Builder::from_resource(
-                "/io/missioncenter/MissionCenter/ui/performance_page/disk_smart_data_entry.ui",
-            );
-
-            let _ = self.row_entry.set(
-                sidebar_content_builder
-                    .object::<gtk::ListBoxRow>("root")
-                    .expect("Could not find `root` object in details pane"),
-            );
-            let _ = self.id.set(
-                sidebar_content_builder
-                    .object::<gtk::Label>("id")
-                    .expect("Could not find `id` object in details pane"),
-            );
-            let _ = self.attribute.set(
-                sidebar_content_builder
-                    .object::<gtk::Label>("attribute")
-                    .expect("Could not find `attribute` object in details pane"),
-            );
-            let _ = self.value.set(
-                sidebar_content_builder
-                    .object::<gtk::Label>("value")
-                    .expect("Could not find `value` object in details pane"),
-            );
-            let _ = self.pretty.set(
-                sidebar_content_builder
-                    .object::<gtk::Label>("pretty")
-                    .expect("Could not find `pretty` object in details pane"),
-            );
-            let _ = self.normalized.set(
-                sidebar_content_builder
-                    .object::<gtk::Label>("normalized")
-                    .expect("Could not find `normalized` object in details pane"),
-            );
-            let _ = self.threshold.set(
-                sidebar_content_builder
-                    .object::<gtk::Label>("threshold")
-                    .expect("Could not find `threshold` object in details pane"),
-            );
-            let _ = self.worst.set(
-                sidebar_content_builder
-                    .object::<gtk::Label>("worst")
-                    .expect("Could not find `worst` object in details pane"),
-            );
-        }
-    }
-*/
     impl WidgetImpl for SmartDialogRow {}
 }
 
@@ -174,6 +90,7 @@ pub struct SmartDialogRowBuilder {
     flags: u16,
 }
 
+// todo remove the builder, this isnt java
 impl SmartDialogRowBuilder {
     pub fn new() -> Self {
         Self {
@@ -225,32 +142,6 @@ impl SmartDialogRowBuilder {
     }
 
     pub fn build(self) -> SmartDialogRow {
-/*        let this = SmartDialogRow::new();
-        {
-            let this = this.imp();
-
-            this.id.get().expect("damn").set_label(format!("{}", self.id).as_str());
-            this.worst.get().expect("Damn").set_label(format!("{}", self.worst).as_str());
-
-            this.value.get().expect("Damn").set_label(format!("{}", self.value).as_str());
-
-            this.pretty.get().expect("Damn").set_label(&match self.units {
-                0 => i18n("N/A"),
-                2 => crate::to_human_readable_time(self.pretty as u64 / 1000),
-                3 => i18n_f("{} sectors", &[&format!("{}", self.pretty)]),
-                4 => i18n_f("{} °C", &[&format!("{}", (self.pretty - 273150) / 1000)]),
-                _ => format!("{}", self.pretty),
-            });
-
-            // this.pretty.get().expect("Damn").set_label(format!("{}", self.pretty).as_str());
-            this.threshold.get().expect("Damn").set_label(format!("{}", self.threshold).as_str());
-            this.normalized.get().expect("Damn").set_label(format!("{}", self.normalized).as_str());
-
-            this.attribute.get().expect("Damn").set_label(self.attribute.as_str());
-        }
-*/
-        // this
-
         SmartDialogRow::new(self.id, self.attribute, self.value, self.pretty, self.units, self.threshold, self.worst, &match self.flags & 0b1 { 1 => i18n("Pre-Fail"), _ => i18n("Old-Age")}, &match self.flags & 0b10 >> 1 { 0 => i18n("Online"), _ => i18n("Offline")}, "IDK LMAO")
     }
 }
