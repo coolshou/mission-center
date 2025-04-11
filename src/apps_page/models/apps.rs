a4d5115b89a057275d86a2d5fe59a6e62c832150
Romeo Calota <mail@romeocalota.me>
2025-04-11 07:16:40 +0300

    Better apps

--- src/apps_page/models/apps.rs
+++ src/apps_page/models/apps.rs
@@ -18,7 +18,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use adw::prelude::*;
 use glib::g_critical;
@@ -26,7 +26,7 @@ use gtk::{gio, glib};
 
 use magpie_types::apps::icon::Icon;
 use magpie_types::apps::App;
-use magpie_types::processes::Process;
+use magpie_types::processes::{Process, ProcessUsageStats};
 
 use crate::apps_page::row_model::{ContentType, RowModel, RowModelBuilder};
 
@@ -53,24 +53,13 @@ pub fn update(
 
         to_remove.push(i);
     }
-    for i in to_remove {
-        list.remove(i);
+    for i in &to_remove {
+        list.remove(*i);
     }
 
     for app in app_map.values() {
-        // Find the first process that has any children. This is most likely the root
-        // of the App's process tree.
-        let mut primary_pid = 0;
-        for (index, pid) in app.pids.iter().enumerate() {
-            if let Some(process) = process_map.get(pid) {
-                if process.children.len() > 0 || index == app.pids.len() - 1 {
-                    primary_pid = process.pid;
-                    break;
-                }
-            }
-        }
-
-        if primary_pid == 0 {
+        let primary_processes = primary_processes(app, process_map);
+        if primary_processes.is_empty() {
             g_critical!(
                 "MissionCenter::AppsPage",
                 "Failed to find primary PID for app {}",
@@ -78,16 +67,6 @@ pub fn update(
             );
             continue;
         }
-
-        let Some(primary_process) = process_map.get(&primary_pid) else {
-            g_critical!(
-                "MissionCenter::AppsPage",
-                "Failed to find primary PID {} for app {}",
-                primary_pid,
-                app.name
-            );
-            continue;
-        };
 
         let row_model = if let Some(index) = list.find_with_equal_func(|obj| {
             let Some(row_model) = obj.downcast_ref::<RowModel>() else {
@@ -105,17 +84,8 @@ pub fn update(
                 .content_type(ContentType::App)
                 .id(&app.id)
                 .build();
-            if let Some(process_model) = process_model_map.get(&primary_pid) {
-                row_model.set_children(process_model.children());
-            }
             list.append(&row_model);
             row_model
-        };
-
-        let usage_stats = if use_merged_stats {
-            primary_process.merged_usage_stats(&process_map)
-        } else {
-            primary_process.usage_stats
         };
 
         let icon = app
@@ -128,11 +98,59 @@ pub fn update(
             })
             .unwrap_or("application-x-executable");
 
-        app_icons.insert(primary_pid, icon.to_string());
+        to_remove.clear();
+        let app_children = row_model.children();
+        for i in (0..app_children.n_items()).rev() {
+            let Some(child) = app_children
+                .item(i)
+                .and_then(|obj| obj.downcast::<RowModel>().ok())
+            else {
+                to_remove.push(i);
+                continue;
+            };
+
+            if primary_processes.iter().any(|p| p.pid == child.pid()) {
+                continue;
+            }
+
+            to_remove.push(i);
+        }
+        for i in &to_remove {
+            app_children.remove(*i);
+        }
+
+        let mut usage_stats = ProcessUsageStats::default();
+        for process in &primary_processes {
+            usage_stats.merge(
+                &(if use_merged_stats {
+                    process.merged_usage_stats(&process_map)
+                } else {
+                    process.usage_stats
+                }),
+            );
+
+            app_icons.insert(process.pid, icon.to_string());
+
+            if app_children
+                .find_with_equal_func(|p| {
+                    if let Some(obj) = p.downcast_ref::<RowModel>() {
+                        obj.pid() == process.pid
+                    } else {
+                        false
+                    }
+                })
+                .is_some()
+            {
+                continue;
+            }
+
+            if let Some(row_model) = process_model_map.get(&process.pid) {
+                app_children.append(row_model);
+            }
+        }
 
         row_model.set_name(app.name.as_str());
         row_model.set_icon(icon);
-        row_model.set_pid(primary_pid);
         row_model.set_cpu_usage(usage_stats.cpu_usage);
         row_model.set_memory_usage(usage_stats.memory_usage);
         row_model.set_shared_memory_usage(usage_stats.shared_memory_usage);
@@ -141,4 +159,39 @@ pub fn update(
         row_model.set_gpu_usage(usage_stats.gpu_usage);
         row_model.set_gpu_memory_usage(usage_stats.gpu_memory_usage);
     }
+}
+
+fn primary_processes<'a>(app: &App, process_map: &'a HashMap<u32, Process>) -> Vec<&'a Process> {
+    let mut secondary_processes = HashSet::new();
+    for app_pid in app.pids.iter() {
+        if let Some(process) = process_map.get(app_pid) {
+            for child in &process.children {
+                if app.pids.contains(child) {
+                    secondary_processes.insert(*child);
+                }
+            }
+        }
+    }
+
+    let mut primary_processes = Vec::new();
+    for app_pid in app.pids.iter() {
+        if let Some(process) = process_map.get(app_pid) {
+            if !secondary_processes.contains(&process.pid) {
+                primary_processes.push(process);
+            }
+        }
+    }
+
+    if primary_processes.is_empty() {
+        for (index, pid) in app.pids.iter().enumerate() {
+            if let Some(process) = process_map.get(pid) {
+                if process.children.len() > 0 || index == app.pids.len() - 1 {
+                    primary_processes.push(process);
+                    break;
+                }
+            }
+        }
+    }
+
+    primary_processes
 }
